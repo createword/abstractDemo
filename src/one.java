4d1be7d157b3d95613967f01d0e173b5f819cf5d
createword <229458869qq.com>
2016-06-23 19:39:49 +0800

    first common

---
+++ src/one.java
@@ -0,0 +1,10 @@
+public abstract class one {
+
+	public  one() {
+		int i = 5;
+		init(5);
+	}
+
+	public abstract void init(int i);
+
+}
---
+++ src/two.java
@@ -0,0 +1,11 @@
+public class two extends one {
+	int t;
+
+	public void init(int i) {
+		t = i;
+	}
+
+	public void getPara() {
+		System.out.println(t);
+	}
+}
